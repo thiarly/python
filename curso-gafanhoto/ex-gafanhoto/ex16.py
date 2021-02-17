e0dae2e3f26c8ff2e4914e0b60ee4951ac3a66d0
thiarly <thiarly.cavalcante@live.com>
2021-02-17 11:35:42 -0300

    create ex16

---
+++ curso-gafanhoto/ex-gafanhoto/ex16.py
@@ -0,0 +1,21 @@
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+#EX: O número 8.874 tem a parte inteira 8.
+
+#import math as math
+num = float(input('Digite um número: '))
+#print ('O valor digitado foi {} e a sua porção inteira é: {}'.format(num, math.trunc(num)))
+print ('O valor digitado foi {} e a sua porção inteira é: {}'.format(num, int(num)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
