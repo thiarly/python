227b63737eabb0e4823b54e30ee3a9ca327abc85
thiarly <thiarly.cavalcante@live.com>
2021-02-19 21:49:40 -0300

    Update ex33.py

--- curso-gafanhoto/aulas-gafanhoto/aula 10/ex33.py
+++ curso-gafanhoto/aulas-gafanhoto/aula 10/ex33.py
@@ -4,13 +4,14 @@ a = float(input('Digite o 1º número: '))
 b = float(input('Digite o 2º número: '))
 c = float(input('Digite o 3º número: '))
 
+#Descobrindo o menor número
 menor = a 
 if b < a and b < c:
     menor = b
 if c < a and c < b:
     menor = c
 
-
+#Descobrindo o maio número
 maior = a
 if b > a and b > c:
     maior = b
