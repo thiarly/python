548ffd77b1a93ec0816cb53dbe0349ec5ae4222a
thiarly <thiarly.cavalcante@live.com>
2021-02-22 17:16:04 -0300

    prova-mundo1

---
+++ curso-gafanhoto/aulas-gafanhoto/aula 12/ex41.py
@@ -0,0 +1,6 @@
+# A confederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
+# até 9 anos: MIRIM
+# até 14 anos: INFANTIL
+# até 19 anos: JUNIOR
+# até 20 anos: SÊNIOR
+# acima: MASTER
