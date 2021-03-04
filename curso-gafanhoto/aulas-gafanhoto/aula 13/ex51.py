e374fd33e94ef620bbdc1d186f897ee838f27737
thiarly <thiarly.cavalcante@live.com>
2021-03-04 17:19:14 -0300

    revisão03.aula12

--- curso-gafanhoto/aulas-gafanhoto/aula 13/ex51.py
+++ curso-gafanhoto/aulas-gafanhoto/aula 13/ex51.py
@@ -1,1 +1,17 @@
-#Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+"""
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. 
+No final, mostre os 10 primeiros termos dessa progressão.
+"""
+
+primeiro = int(input('Primeiro Termo: '))
+razão = int(input('Razão: '))
+
+decimo = primeiro + (10 - 1 ) * razão
+
+print (decimo)
+
+for cont in range(primeiro, decimo + razão, razão):
+    print('{}'.format(cont), end= '-> ')
+
+print ('\033[31mACABOU!\033[m')
+
--- curso-gafanhoto/aulas-gafanhoto/aula 13/ex52.py
+++ curso-gafanhoto/aulas-gafanhoto/aula 13/ex52.py
@@ -1,1 +1,17 @@
-#Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+"""
+Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+"""
+tot = 0
+num = int(input('Digite um número: '))
+
+for c in range(1, num +1):
+    if num % c == 0:
+        print ('\033[31m', end=' ')
+        tot += 1
+    else:
+        print('\033[32m', end=' ')
+    print('{}\033[m'.format(c), end=' ')
+
+print ('\nO número \033[35m{}\033[m foi divisível \033[34m{}\033[m vezes.'.format(num, tot))
+print (num)
+
