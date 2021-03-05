b550b4e6b5a53e2a0fc17cfcd4a360b477cf8b2e
thiarly <thiarly.cavalcante@live.com>
2021-03-05 16:28:30 -0300

    aula14

---
+++ curso-gafanhoto/aulas-gafanhoto/aula 14/ex59.py
@@ -0,0 +1,11 @@
+"""
+Crie um programa que leia dois valores e mostre um menu na tela:
+
+[1] somar
+[1] multiplicar
+[1] maior
+[1] novos números
+[5] sair do programa
+
+Seu programa deverá realizar a operação solicitada em cada caso.
+"""
---
+++ curso-gafanhoto/aulas-gafanhoto/aula 14/ex63.py
@@ -0,0 +1,6 @@
+"""
+Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primos elementos
+de uma SEQUÊNCIA DE FIBONANCCI.
+EX:
+0-> 1-> 1-> 2-> 3-> 5-> 8 
+"""
