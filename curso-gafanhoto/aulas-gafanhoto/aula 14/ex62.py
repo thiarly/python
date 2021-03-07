70964268ab3b2b04f92f7b1f29692b0032b593fa
thiarly <thiarly.cavalcante@live.com>
2021-03-07 11:56:07 -0300

    ex62,63,64,65

--- curso-gafanhoto/aulas-gafanhoto/aula 14/ex62.py
+++ curso-gafanhoto/aulas-gafanhoto/aula 14/ex62.py
@@ -2,3 +2,25 @@
 Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
 O programa encerra quando ele disser que quer mostrar 0 termos.
 """
+
+
+
+print ('-='*8)
+print ('GERADOR DE PA')
+print ('-='*8)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print ('{} → '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? : '))
+print('A progressão foi finaliza com o total {} termos.'.format(total))
--- curso-gafanhoto/aulas-gafanhoto/aula 14/ex64.py
+++ curso-gafanhoto/aulas-gafanhoto/aula 14/ex64.py
@@ -5,3 +5,20 @@ foram digitados e qual foi a soma entre eles.
 
 (Desconsiderando o flag 999)
 """
+
+num = cont = soma = 0
+num = int(input('Digite um número: '))
+while num != 999:
+    cont = cont + 1
+    soma = soma + num
+    num = int(input('Digite um número: '))
+print ('Acabou! Você digitou tantos números: {} e a soma foi {}'.format(cont, soma))
+
+"""
+num = cont = soma = 0
+while num != 999:
+    num = int(input('Digite um número: '))
+    cont = cont + 1
+    soma = soma + num
+print ('Acabou! Você digitou tantos números: {} e a soma foi {}'.format(cont -1, soma -999))
+"""
