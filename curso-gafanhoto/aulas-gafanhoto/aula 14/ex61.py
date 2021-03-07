2a88e651d94d265e8aed10d4c5f959f8c87b5901
thiarly <thiarly.cavalcante@live.com>
2021-03-06 21:27:31 -0300

    ex59,60,61

--- curso-gafanhoto/aulas-gafanhoto/aula 14/ex61.py
+++ curso-gafanhoto/aulas-gafanhoto/aula 14/ex61.py
@@ -2,3 +2,21 @@
 Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos
 da progressão usando a estrutura while.
 """
+
+
+print ('-='*8)
+print ('GERADOR DE PA')
+print ('-='*8)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+
+
+while cont < 10:
+    print ('{} → '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
+
+
