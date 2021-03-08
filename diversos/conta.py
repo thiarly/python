12e826f600677a4bbc5c2e96c978fc691a794e28
thiarly <thiarly.cavalcante@live.com>
2021-03-08 18:27:21 -0300

    at01.suplementos

--- diversos/conta.py
+++ diversos/conta.py
@@ -24,27 +24,27 @@ while True:
     s = (s + suplementação)
 print (s)
 
-
+##################################################################################
 treinador = float(input('Digite o valor do treinador R$: '))
 fisioterapia = float(input('Digite o valor da fisioterapia R$: '))
 material = float(input('Digite o valor dos materiais R$: '))
-
+##################################################################################
 tre = treinador
 sup = suplementação
 fis = fisioterapia
 mat = material
 soma = treinador + suplementação + fisioterapia
-
+##################################################################################
 p1 = (sup / soma) * 100
 p2 = (fis / soma) * 100
 p3 = (tre / soma) * 100
 p4 = (mat / soma) * 100
-
-
+##################################################################################
 print ('\033[034mSuplemento R$ {:.2f} / ({:.2f}%)\033[m'.format(sup, p1))
 print ('\033[033mFisioterapia R$ {:.2f} / ({:.2f}%)\033[m'.format(fis, p2))
 print ('\033[032mTreinador R$ {:.2f} / ({:.2f}%)\033[m'.format(tre, p3))
 print ('\033[031mValor total R$ {:.2f}\033[m'.format(soma))
+##################################################################################
 
 
 
