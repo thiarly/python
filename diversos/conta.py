69eae27bbc49eede4cc242d3c9e99c16b1965803
thiarly <thiarly.cavalcante@live.com>
2021-03-08 18:23:56 -0300

    aula15

--- diversos/conta.py
+++ diversos/conta.py
@@ -6,8 +6,6 @@ energy1 = 47.12
 energy2 = 47.12
 """
 
-
-
 from time import sleep
 
 print ('-˜='*20)
@@ -15,9 +13,19 @@ print ('\033[032mCONTA ESPORTE MENSAL\033[m')
 print ('-˜='*20)
 
 sleep(2)
+cont = 0
+
+s = 0
+while True:
+    suplementação = float(input('Digite o valor dos suplementos R$: '))
+    confirma = str(input('Deseja adicionar mais algum suplemento? [S/N]: ')).strip().upper()[0]
+    if confirma in 'Nn':
+        break
+    s = (s + suplementação)
+print (s)
+
 
 treinador = float(input('Digite o valor do treinador R$: '))
-suplementação = float(input('Digite o valor dos suplementos R$: '))
 fisioterapia = float(input('Digite o valor da fisioterapia R$: '))
 material = float(input('Digite o valor dos materiais R$: '))
 
@@ -37,5 +45,7 @@ print ('\033[034mSuplemento R$ {:.2f} / ({:.2f}%)\033[m'.format(sup, p1))
 print ('\033[033mFisioterapia R$ {:.2f} / ({:.2f}%)\033[m'.format(fis, p2))
 print ('\033[032mTreinador R$ {:.2f} / ({:.2f}%)\033[m'.format(tre, p3))
 print ('\033[031mValor total R$ {:.2f}\033[m'.format(soma))
+
+
 
 
