723382d0a23d16013c184181a9b66b3e5ced5446
thiarly <thiarly.cavalcante@live.com>
2021-03-09 17:27:42 -0300

    exaula15

--- cursos/cursoemvideo/aula 15/ex67.py
+++ cursos/cursoemvideo/aula 15/ex67.py
@@ -3,3 +3,20 @@ Faça um programa que mostre a tabuada de vários números, um de cada vez, para
 digitado pelo usuário.
 O Programa será interrompido quando o número solicitado for negativo.
 """
+
+
+while True:
+    n = int(input('Digite um número para ver a sua tabuada: '))
+    print ('˜-'*20)
+    if n < 0:
+       break
+    for c in range(1, 11):
+        print (f'{n} x {c} = {n * c}')
+    print ('˜-'*20)
+    conf = ('Deseja fazer a tabuada de outro número? [S/N]').strip().upper()[0]
+    
+
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
+    
+
+
--- cursos/cursoemvideo/aula 15/ex71.py
+++ cursos/cursoemvideo/aula 15/ex71.py
@@ -4,3 +4,39 @@ Crie um programa que simule o funcionamento de um caixa eletrônico. No inicio, 
 
 OBS.: Considere que o caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1
 """
+
+print('=' * 30)
+print(f'{"BANCO DE CANUDOS":^30}')
+print('=' * 30)
+valor = float(input('Qual valor deseja sacar? '))
+if valor < 1:
+    print ('Valor inválidos! Os saques disponíves estão a parti de R$ 5 a R$ 100.')
+    valor = float(input('Digite o valor novamente a sacar: '))
+    
+total = valor
+ced = 100
+totced = 0
+
+while True:
+    if total >= ced:
+        total = total - ced
+        totced = totced + 1
+    else:
+        if totced > 0:
+            print (f'Total de {totced} cédulas de R$ {ced}')
+        if ced == 100:
+            ced = 50
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        totced = 0
+        if total == 0:
+            break
+print ('='*30)
+print ('Volte sempre ao BANCO DE CANUDOS, tenha um ótimo dia!')
+
+
+   
