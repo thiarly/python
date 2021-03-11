c38c7215122fd13acc9e7ea87adce190f609789a
thiarly <thiarly.cavalcante@live.com>
2021-03-11 16:08:32 -0300

    exfeito

--- cursos/cursoemvideo/aula 16/ex72.py
+++ cursos/cursoemvideo/aula 16/ex72.py
@@ -1,5 +1,27 @@
 """
-Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vince.
+Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 """
 
+numeros = ('Zero','Um','Dois','Três','Quatro','Cinco',
+'Seis','Sete','Oito','Nove','Dez','Onze','Doze',
+'Treze','Quatorze','Quinze','Dezesseis','Dezessete'
+,'Dezoito','Dezenove','Vinte',)
+
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print ('Número inválido. ', end='')
+print (f'Você digitou o número {numeros[num]}')
+
+
+"""
+pos = -1
+while pos < 0 or pos > 20:
+    pos = int(input('Digite um número entre 0 e 20: '))
+    if pos != 0 or pos != 20:
+        print ('Número inválido tente novamente.') 
+print (f'Você digitou o número: {numeros[pos -1]}')
+"""
