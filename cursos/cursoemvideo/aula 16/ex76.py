818e5abfc04b7910d5d808f392e8ebcbb55b1cca
thiarly <thiarly.cavalcante@live.com>
2021-03-11 17:30:08 -0300

    ex75,8

--- cursos/cursoemvideo/aula 16/ex76.py
+++ cursos/cursoemvideo/aula 16/ex76.py
@@ -2,3 +2,26 @@
 Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequencia.
 No final, mostre uma listagem de preços, organizado os dados em forma tabular.
 """
+
+listagem = ('Caneta', 1.70,
+            'Borracha', 1,
+            'Caderno', 20,
+            'Marca texo', 5.50,
+            'Pincel', 28,
+            'Case Notebook', 250,
+            'Lousa', 120,
+            'Estojo', 5,
+            'Livro', 45)
+
+print ('-'*45)
+print (f'{"LISTAGEM DE PREÇO":^45}')
+print ('-'*45)
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print (f'{listagem[pos]:.<35}',end='')
+    else:
+        print (f'R$ {listagem[pos]:.2f}')
+
+print ('-'*45)
+
--- cursos/cursoemvideo/aula 16/ex77.py
+++ cursos/cursoemvideo/aula 16/ex77.py
@@ -2,3 +2,15 @@
 Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 """
+
+palavras = ('aprender', 'programar', 'linguagem', 'python',
+            'esporte', 'natacao', 'bike', 'run',
+            'em', 'busca', 'liberdade', 'financeira')
+
+for c in palavras:
+    print (f'\nNa palavra {c.upper()} temos:->  ', end='')
+    for letra in c:
+        if letra.lower() in 'aeiou':
+            print(letra, end= ' ')
+
+
