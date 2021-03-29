d3e9ba292b667169ac96e19554978a0e66d98cea
thiarly <thiarly.cavalcante@live.com>
2021-03-29 16:50:18 -0300

    aula21

---
+++ cursos/cursoemvideo/aula 21/aulapatrica01.py
@@ -0,0 +1,14 @@
+"""
+- Interactive Help
+- Docstrings
+- Parametros opcionais
+- Escopo de váriaveis
+- Retorno de resultados
+"""
+
+#PARAMETROS OPCIONAIS
+def somar (a=0, b=0, c=0):
+    s = a + b + c
+    print (f'A soma vale {s}')
+
+somar (3)
