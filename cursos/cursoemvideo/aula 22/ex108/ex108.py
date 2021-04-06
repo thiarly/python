473301c85ede172dc7c2b74a883288f7de216b90
thiarly <thiarly.cavalcante@live.com>
2021-04-06 17:18:29 -0300

    ex110,ex111,ex112

--- cursos/cursoemvideo/aula 22/ex108/ex108.py
+++ cursos/cursoemvideo/aula 22/ex108/ex108.py
@@ -3,11 +3,11 @@ Adapte o código do desafio 107, criando uma função adicional chamada() que co
 monetário formatdado.
 """
 
-import money
+import moeda
 
 p = float(input('Digite o preço: R$ '))
-print (f'A metade de R$ {money.moeda(p)} é R$ {money.moeda(money.metade(p))}')
-print (f'O dobro de R$ {money.moeda(p)} é R$ {money.moeda(money.dobro(p))}')
-print (f'Aumento o {money.moeda(p)} em 10% {money.moeda(money.aumentar(p, 10))}')
-print (f'O desconto de 10% em R$ {money.moeda(p)} é R$ {money.moeda(money.diminuir(p, 10))}')
-print (f'O valor {money.moeda(p)} multiplicado por 10x é {money.moeda(money.multiplicar(p))}')
+print (f'A metade de R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.metade(p))}')
+print (f'O dobro de R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.dobro(p))}')
+print (f'Aumento o {moeda.moeda(p)} em 10% {moeda.moeda(moeda.aumentar(p, 10))}')
+print (f'O desconto de 10% em R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.diminuir(p, 10))}')
+print (f'O valor {moeda.moeda(p)} multiplicado por 10x é {moeda.moeda(moeda.multiplicar(p))}')
--- cursos/cursoemvideo/aula 22/ex109/ex109.py
+++ cursos/cursoemvideo/aula 22/ex109/ex109.py
@@ -3,11 +3,11 @@ Modifique as funções que foram criadas no desafio 107 para que elas aceitem um
 retornado por elas vai ser ou não formatado pela função moeda(), desenvolvendo no desafio 108.
 """
 
-import cash
+import moeda
 
 p = float(input('Digite o preço: R$ '))
-print (f'A metade de R$ {cash.moeda(p)} é R$ {cash.metade(p, True)}')
-print (f'O dobro de R$ {cash.moeda(p)} é R$ {cash.dobro(p, True)}')
-print (f'Aumento o {cash.moeda(p)} em 10% {cash.aumentar(p, 10, True)}')
-print (f'O desconto de 10% em R$ {cash.moeda(p)} é R$ {cash.diminuir(p, 10, True)}')
-print (f'O valor {cash.moeda(p)} multiplicado por 10x é {cash.multiplicar(p, True)}')
+print (f'A metade de R$ {moeda.moeda(p)} é R$ {moeda.metade(p, True)}')
+print (f'O dobro de R$ {moeda.moeda(p)} é R$ {moeda.dobro(p, True)}')
+print (f'Aumento o {moeda.moeda(p)} em 10% {moeda.aumentar(p, 10, True)}')
+print (f'O desconto de 10% em R$ {moeda.moeda(p)} é R$ {moeda.diminuir(p, 10, True)}')
+print (f'O valor {moeda.moeda(p)} multiplicado por 10x é {moeda.multiplicar(p, True)}')
