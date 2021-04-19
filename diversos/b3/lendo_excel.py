4254c701632f741b31a854303b0e5d3b30b791b0
thiarly <thiarly.cavalcante@live.com>
2021-04-19 15:13:55 -0300

    lendo_excel

---
+++ diversos/b3/lendo_excel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+x = pd.read_excel("/Users/thiarly/Documents/MEUSPROJETOS/python/diversos/b3/base.xlsx")
+
+plt.plot(x["idade"])
+
+plt.show()
+
+
+
+
+
---
+++ diversos/b3/rent_contizada.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = (18,8)
+matplotlib.style.use('seaborn-darkgrid')
