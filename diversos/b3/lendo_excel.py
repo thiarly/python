3bf0f912719eaf7faeaabeecb60fa7edd8fa2628
thiarly <thiarly.cavalcante@live.com>
2021-04-19 15:30:52 -0300

    novos.graficos
    Algumas formas para leitura do excel com gráficos.

--- diversos/b3/lendo_excel.py
+++ diversos/b3/lendo_excel.py
@@ -2,7 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 x = pd.read_excel("/Users/thiarly/Documents/MEUSPROJETOS/python/diversos/b3/base.xlsx")
 
-plt.plot(x["idade"])
+#plt.plot(x["idade"])
+#plt.hist(x["idade"])
+#plt.hist(x["idade"],bins=20)
+plt.pie(x["idade"], labels= x["nome"],autopct="%1.2f%%")
 
 plt.show()
 
