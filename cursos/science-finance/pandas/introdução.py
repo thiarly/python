31a4bbeac7154b5f3d98cd9f9f6ac12e73702731
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2021-05-05 16:29:17 -0300

    leitura excel

--- cursos/science-finance/pandas/introdução.py
+++ cursos/science-finance/pandas/introdução.py
@@ -8,10 +8,4 @@ import pandas as pd
 
 # Obtendo as cotações da VALE3 através de um arquivo CSV remoto
 
-
-
-
-pd.read_csv('/Users/thiarly/Documents/MeusProjetos/Excel/Excel/vale3.csv')
-
-
-
+pd.read_csv('https://github.com/codigoquant/data/blob/main/VALE3.csv')pip
---
+++ cursos/science-finance/pandas/t.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+csv = pd.read_csv('https://raw.githubusercontent.com/codigoquant/data/main/VALE3.csv')
+
+print (csv)
+
