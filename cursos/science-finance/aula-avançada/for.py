e5f76fd343ecfd3387dfa436833e50cb8995f0e4
thiarly <thiarly.cavalcante@live.com>
2021-05-14 15:15:56 -0300

    .

---
+++ cursos/science-finance/aula-avançada/for.py
@@ -0,0 +1,33 @@
+# Zip
+esquerda = [2, 4, 6, 8, 10]
+direita = [3, 6, 9, 12, 13]
+for valor_esq, valor_dir in zip(esquerda, direita):
+    print(valor_esq, valor_dir)
+
+
+# map e filter
+## map pega uma função e a aplica a cada valor de um iterador
+
+# 0 a 9 ao quadrado
+elevar_ao_quadrado = lambda x: x ** 2
+for val in map(elevar_ao_quadrado, range(10)):
+    print(val, end=' ')
+print()
+
+# filter é semelhante, exceto pelo fato de os valores passados serem avaliados como True:
+## Valore pares até 10
+
+eh_par = lambda x: x % 2 == 0
+for val in filter(eh_par, range(10)):
+    print(val, end=' ')
+print()
+
+filtro = lambda x: x > 2
+for val in filter(filtro, range(10)):
+    print(val, end=' ')
+print()
+
+# itertools
+from itertools import permutations
+p = permutations('LUZ')
+print(*p)
--- cursos/science-finance/fonte-dados-financeiros/yfinance.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-msft = yf.Ticker("MSFT")
-
-hist = (msft.history(period="max"))
-
-print(hist)
-
